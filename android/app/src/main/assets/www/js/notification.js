d8696bcf3abda2969619e12464357bf151df8c1d
vu <nam>
2018-08-07 22:54:54 +0700

    remove source

--- android/app/src/main/assets/www/js/notification.js
+++
@@ -1,5 +0,0 @@
-/*global cordova, module*/
-
-window.sum =function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Notification", "Notification", [name]);
-};
